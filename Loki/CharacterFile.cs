085ee3c72df6923d51a18d590bae4d31a7275caf
Adam Rhodes <wuffles236@gmail.com>
2021-03-15 17:11:34 +0000

    Improve character file selection dropdown

--- Loki/CharacterFile.cs
+++ Loki/CharacterFile.cs
@@ -1,13 +1,52 @@
-﻿using System.Globalization;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 namespace Loki
 {
-    public class CharacterFile
+    public class CharacterFile: INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string FilePath { get; set; }
+        private string _playerName;
+        private string _filePath;
+
+        /// <summary>
+        /// Name of player as read from the character profile inside the file.
+        /// </summary>
+        public string PlayerName
+        {
+            get => _playerName;
+            set
+            {
+                if (value == _playerName) return;
+                _playerName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// ID for the player as extracted from within the file.
+        /// </summary>
+        public long PlayerId { get; set; }
+
+        public string FilePath
+        {
+            get => _filePath;
+            set
+            {
+                if (value == _filePath) return;
+                _filePath = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Set if failed to load from disk e.g. incompatible format etc.
+        /// </summary>
+        public bool Invalid { get; set; }
 
         public static CharacterFile[] LoadCharacterFiles()
         {
@@ -19,11 +58,32 @@ namespace Loki
         public static CharacterFile FromPath(string characterFilePath)
         {
             string name = Path.GetFileNameWithoutExtension(characterFilePath);
-            name = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name.ToLower());
-            return new CharacterFile
-                {FilePath = characterFilePath, Name = name};
+
+            try
+            {
+                using var fileStream = File.OpenRead(characterFilePath);
+                PlayerProfile p = PlayerProfile.Read(fileStream);
+
+                return new CharacterFile
+                    {FilePath = characterFilePath, PlayerName = p.PlayerName, PlayerId = p.PlayerId};
+            }
+            catch (Exception ex)
+            {
+                // TODO: Log error.
+                return new CharacterFile
+                {
+                    FilePath = characterFilePath, PlayerName = "<unknown>", Invalid = true,
+                };
+            }
         }
 
-        public override string ToString() => $"{Name} [{FilePath}]";
+        public override string ToString() => $"{PlayerName} [{FilePath}]";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Loki/MainWindow.xaml.cs
+++ Loki/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace Loki
             {
                 CharacterFiles = await Task.Run(CharacterFile.LoadCharacterFiles);
                 SelectedCharacterFile = CharacterFiles.FirstOrDefault();
+                CommandManager.InvalidateRequerySuggested();
             }
             catch (Exception ex)
             {
@@ -129,10 +130,41 @@ namespace Loki
                     if (makeBackup) 
                         Backup.BackupCharacter(character);
 
+                    // TODO: Add some logging in here so easy to track down what went wrong should this break horribly.
+                    
+                    // WIP:
+                    // TODO: Renaming file - should probably make this a setting or tickbox somewhere
+                    // Using this feature is a bad idea with steam cloud synchronisation that Valheim uses - it will just restore their old character leaading to confusion.
+
+                    /*
+                    // See if we should use a new file path, if the name has changed.
+                    string fileName = Path.GetFileNameWithoutExtension(character.FilePath);
+                    if(fileName != profile.PlayerName)
+                    {
+                        // Check character name isn't dodgy for a filename.
+                        bool dodgyName = Path.GetInvalidFileNameChars()
+                            .Any(invalidChar => profile.PlayerName.Contains(invalidChar));
+
+                        // If it looks OK and doesn't exist, rename old file.
+                        if (!dodgyName)
+                        {
+                            string newFilePath = Path.Join(Path.GetDirectoryName(character.FilePath),
+                                profile.PlayerName + ".fch");
+                            if (!File.Exists(newFilePath))
+                            {
+                                File.Move(character.FilePath, newFilePath);
+                                character.FilePath = newFilePath;
+                            }
+                        }
+                    }
+                   */
+
                     using var fileStream = File.Create(character.FilePath);
+
                     profile.Write(fileStream);
                 });
 
+                character.PlayerName = Profile.PlayerName;
                 ShowNotification("Character Saved");
             }
             catch (Exception ex)
@@ -157,7 +189,7 @@ namespace Loki
 
         private void CanSaveOrRevertExecute(object sender, CanExecuteRoutedEventArgs e)
         {
-            e.CanExecute = SelectedCharacterFile != null && !SaveInProgress;
+            e.CanExecute = SelectedCharacterFile != null && !SelectedCharacterFile.Invalid && !SaveInProgress;
         }
 
         private void RevertExecuted(object sender, ExecutedRoutedEventArgs e)
