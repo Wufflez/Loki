02f71382fdb5a25a3a027cd3bdf49729ea40fff9
Adam Rhodes <wuffles236@gmail.com>
2021-03-15 19:41:17 +0000

    Disable main controls when saving

---
+++ Loki/InverseBooleanConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Loki
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is bool boolValue ? !boolValue : value;
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Convert(value, targetType, parameter, culture);
+    }
+}
