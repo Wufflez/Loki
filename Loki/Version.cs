4dbaf4b92fa6f37036f4a6d5f81fd5a536cfb12a
Björn Domars <bdomars@0xbd.fi>
2022-11-27 21:54:51 +0200

    Update profile version and compatibility check

--- Loki/Version.cs
+++ Loki/Version.cs
@@ -8,13 +8,14 @@ namespace Loki
         /// <summary>
         /// Version of PlayerProfile .fch data. This is the version of the saved file.
         /// </summary>
-        public const int ProfileVersion = 36;
+        public const int ProfileVersion = 37;
 
         /// <summary>
         /// These legacy versions are also considered compatible, in addition to <see cref="ProfileVersion"/>
         /// </summary>
         private static readonly int[] CompatibleProfileVersions =
         {
+            36,
             35,
             34,
             33,
